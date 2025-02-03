102d925bbd5c8c870b7ee1bbe7be0caf67ab2b89
CodeGoat <levijohnson2008@live.co.uk>
2025-02-03 18:22:36 +0000

    Chore: Bump library version
    Bumps the library version to v1.1.2.

--- src/NetworkManager.py
+++ src/NetworkManager.py
@@ -1,5 +1,5 @@
 # Goat - Pico Network Manager library
-# Version 1.1.1
+# Version 1.1.2
 # © (c) 2024-2025 Goat Technologies
 # Description:
 # Provides network management for your device firmware.
@@ -33,7 +33,7 @@ class NetworkManager:
         self.config_file = "network_config.conf"
 
         # Constants
-        self.VERSION = "1.1.1"
+        self.VERSION = "1.1.2"
         self.repo_url = "https://github.com/CodeGoat-dev/Pico-Network-Manager"
 
         # Interface configuration
